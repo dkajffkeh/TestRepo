fe909fe76a72416e4c9f7e511d416436faccd79b
PatrickYoo <yhy1045@naver.com>
2020-11-22 21:53:14 +0900

    ,,

--- src/com/home/TestServlet.java
+++ src/com/home/TestServlet.java
@@ -28,9 +28,13 @@ public class TestServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 	System.out.println("test");
-	
-	
-	
+	System.out.println("test");
+	System.out.println("test3");
+	System.out.println("test3");
+	System.out.println("test3");
+	System.out.println("test3");
+	System.out.println("test3");
+
 
 	}
 	/**
